61adc1d284707b47872cdc6f7221c0bd32c355eb
Nalule Esther Precious <estherprecious473@gmail.com>
2025-06-11 18:21:14 +0300

    Add files via upload

---
+++ Differentiating between assigning and declaring a variable with a practical example of each.py
@@ -0,0 +1,7 @@
+Assigning a variable means giving a value to the declared variable
+Example
+name = "Esther" 
+
+Declaring a variable means telling the program about the existence of a variable, usually by specifying its name and its type
+Example
+name = None
